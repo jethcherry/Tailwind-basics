94a3cb307c541e09f8b6dfffba859e6026857410
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-17 09:29:57 +0300

    added flex items

--- src/component/Tailwind.tsx
+++ src/component/Tailwind.tsx
@@ -4,10 +4,10 @@ function Tailwind() {
       {/* <div className=" container bg-red-500 w-40 h-40 ">
         This is a div element
       </div> */}
-      <div className="bg-yellow-500 w-auto flex flex-row flex-wrap">
-        <div className="h-10 w-80 bg-red-500">1</div>
-        <div className="h-10 w-80 bg-blue-500">2</div>
-        <div className="h-10 w-80 bg-green-500">3</div>
+      <div className="bg-yellow-500  flex">
+        <div className=" flex-shrink-0 h-10 w-80 bg-red-500">1</div>
+        <div className="flex-shrink-0 h-10 w-80 bg-blue-500">2</div>
+        <div className="flex-shrink-0 h-10 w-80 bg-green-500">3</div>
       </div>
     </div>
   );
