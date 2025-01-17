c1a4cc952241e42c52505d3d961d1070a96b8392
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-17 08:49:53 +0300

    how flexbox works

--- src/component/Tailwind.tsx
+++ src/component/Tailwind.tsx
@@ -1,7 +1,14 @@
 function Tailwind() {
   return (
     <div>
-      <div className=" container bg-red-500">This is a div element </div>
+      {/* <div className=" container bg-red-500 w-40 h-40 ">
+        This is a div element
+      </div> */}
+      <div className="bg-yellow-500 w-auto flex flex-row flex-wrap">
+        <div className="h-10 w-80 bg-red-500">1</div>
+        <div className="h-10 w-80 bg-blue-500">2</div>
+        <div className="h-10 w-80 bg-green-500">3</div>
+      </div>
     </div>
   );
 }
