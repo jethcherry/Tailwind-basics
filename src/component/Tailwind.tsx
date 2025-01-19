4c9083c8c2d5e9840db6be65172232e2a0b8a7f6
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-19 11:54:10 +0300

    creating a grid layout

--- src/component/Tailwind.tsx
+++ src/component/Tailwind.tsx
@@ -1,7 +1,7 @@
 function Tailwind() {
   return (
     <div className="bg-yellow-500 w-auto grid grid-cols-3 gap-4">
-      <div className=" h-20  bg-red-500">1</div>
+      <div className="col-span-2 h-20  bg-red-500">1</div>
       <div className=" h-20  bg-blue-500">2</div>
       <div className=" h-20  bg-green-500">3</div>
 
