3f8ff939677e5cfef92c14bfed32b93f435605d1
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-19 12:10:00 +0300

    creating row layout

--- src/component/Tailwind.tsx
+++ src/component/Tailwind.tsx
@@ -1,7 +1,21 @@
 function Tailwind() {
   return (
-    <div className="bg-yellow-500 w-auto grid grid-cols-3 gap-4">
-      <div className="col-span-2 h-20  bg-red-500">1</div>
+    // <div className="bg-yellow-500 w-auto grid grid-cols-3 gap-4">
+    //   <div className=" h-20  bg-red-500">1</div>
+    //   <div className=" h-20  bg-blue-500">2</div>
+    //   <div className=" h-20  bg-green-500">3</div>
+
+    //   <div className=" h-20  bg-red-500">4</div>
+    //   <div className=" h-20  bg-blue-500">5</div>
+    //   <div className=" h-20  bg-green-500">6</div>
+
+    //   <div className=" h-20  bg-red-500">7</div>
+    //   <div className=" h-20  bg-blue-500">8</div>
+    //   <div className=" h-20  bg-green-500">10</div>
+    // </div>
+
+    <div className="bg-yellow-500 w-auto grid grid-rows-3 grid-flow-col gap-4">
+      <div className="row-span-2 h-20  bg-red-500">1</div>
       <div className=" h-20  bg-blue-500">2</div>
       <div className=" h-20  bg-green-500">3</div>
 
@@ -11,7 +25,7 @@ function Tailwind() {
 
       <div className=" h-20  bg-red-500">7</div>
       <div className=" h-20  bg-blue-500">8</div>
-      <div className=" h-20  bg-green-500">10</div>
+      <div className=" h-20  bg-green-500">9</div>
     </div>
   );
 }
