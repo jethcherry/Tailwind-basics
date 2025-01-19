45803a78254ef3f3e30ac1a1eda0bd9c7140dc22
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-19 15:49:10 +0300

    adding the gap btw and justifying items

--- src/component/Tailwind.tsx
+++ src/component/Tailwind.tsx
@@ -14,19 +14,33 @@ function Tailwind() {
     //   <div className=" h-20  bg-green-500">10</div>
     // </div>
 
-    <div className="bg-yellow-500 w-auto grid grid-cols-5 grid-flow-row gap-8">
-      <div className=" h-20  bg-red-500">1</div>
-      <div className=" h-20  bg-blue-500">2</div>
-      <div className=" h-20  bg-green-500">3</div>
+    // <div className="bg-yellow-500 w-auto grid grid-cols-5 grid-flow-row gap-8">
+    //   <div className=" h-20  bg-red-500">1</div>
+    //   <div className=" h-20  bg-blue-500">2</div>
+    //   <div className=" h-20  bg-green-500">3</div>
 
-      <div className=" h-20  bg-red-500">4</div>
-      <div className=" h-20  bg-blue-500">5</div>
-      <div className=" h-20  bg-green-500">6</div>
+    //   <div className=" h-20  bg-red-500">4</div>
+    //   <div className=" h-20  bg-blue-500">5</div>
+    //   <div className=" h-20  bg-green-500">6</div>
 
-      <div className=" h-20  bg-red-500">7</div>
-      <div className=" h-20  bg-blue-500">8</div>
-      <div className=" h-20  bg-green-500">9</div>
-    </div>
+    //   <div className=" h-20  bg-red-500">7</div>
+    //   <div className=" h-20  bg-blue-500">8</div>
+    //   <div className=" h-20  bg-green-500">9</div>
+    // </div>
+
+    <div className="bg-yellow-500 w-auto grid grid-cols-5 grid-flow-row gap-x-4 gap-y-4">
+    <div className=" h-20  bg-red-500">1</div>
+    <div className=" h-20  bg-blue-500">2</div>
+    <div className=" h-20  bg-green-500">3</div>
+
+    <div className=" h-20  bg-red-500">4</div>
+    <div className=" h-20  bg-blue-500">5</div>
+    <div className=" h-20  bg-green-500">6</div>
+
+    <div className=" h-20  bg-red-500">7</div>
+    <div className=" h-20  bg-blue-500">8</div>
+    <div className=" h-20  bg-green-500">9</div>
+  </div>
   );
 }
 
