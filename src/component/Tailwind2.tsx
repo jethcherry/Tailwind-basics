7c6819fe2d064670df410d04b9bf817a8bf742db
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-20 10:46:22 +0300

    styling the borders

--- src/component/Tailwind2.tsx
+++ src/component/Tailwind2.tsx
@@ -4,9 +4,9 @@ function Tailwind2() {
     //   <div className="text-4xl text-center text-green-700 font-light lowercase">
     //     This is the inner div
     //   </div>
-    // </div>
+    // </div> borde
     <div>
-      <button className="bg-green-600 m-20 p-5 text-white rounded-t-md">
+      <button className="bg-green-600 m-20 p-5 text-white rounded-t-md border-white border-8 border-opacity-100 border-dashed">
         Click Here
       </button>
     </div>
