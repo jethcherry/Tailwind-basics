48f680c5e49f54283f022a2456fd7e7343835f02
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-01-20 09:45:22 +0300

    added border radius to elements

--- src/component/Tailwind2.tsx
+++ src/component/Tailwind2.tsx
@@ -1,9 +1,14 @@
 function Tailwind2() {
   return (
-    <div className="">
-      <div className="text-4xl text-center text-green-700 font-light lowercase">
-        This is the inner div
-      </div>
+    // <div className="">
+    //   <div className="text-4xl text-center text-green-700 font-light lowercase">
+    //     This is the inner div
+    //   </div>
+    // </div>
+    <div>
+      <button className="bg-green-600 m-20 p-5 text-white rounded-t-md">
+        Click Here
+      </button>
     </div>
   );
 }
